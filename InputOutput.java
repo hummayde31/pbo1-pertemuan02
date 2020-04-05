21313c49179a895193e94aca3f893d9f5a8d55f4
hummayde31 <62832964+hummayde31@users.noreply.github.com>
2020-04-05 19:30:28 +0800

    update

--- InputOutput.java
+++ InputOutput.java
@@ -4,7 +4,7 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
-  double Nilai_tugas;
+  double Nilai_tugas , Nilai_kuis , Nilai_uts , Nilai_uas ;
   
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
@@ -12,6 +12,18 @@ public class InputOutput{
 
   System.out.print("Masukkan Nilai tugas : ");
   Nilai_tugas = scanner.nextInt();
+
+  System.out.print("Nilai kuis : ");
+  Nilai_kuis = scanner.nextInt();
+
+  System.out.print("Nilai uts : ");
+  Nilai_uts = scanner.nextInt();
+
+  System.out.print("Nilai uas : ");
+  Nilai_uas = scanner.nextInt();
+
+  Nilai_akhir = (0.15 * n_tugas + 0.20 * n_kuis + 0.30 * n_uts + 0.35 * n_uas);
+  System.out.println(nama+" memiliki nilai akhir "+n_akhir);
 
  }
 }
